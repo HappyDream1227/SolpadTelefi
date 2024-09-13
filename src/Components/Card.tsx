7c12d7b216ce6a6fb76ffa78c4ec5b95843ac3c1
HappyDream1227 <davidann1227@gmail.com>
2024-09-13 15:10:19 +0900

    second commit

---
+++ src/Components/Card.tsx
@@ -0,0 +1,25 @@
+// this is card component with rounded gradient border. It can hold children components.
+import { ReactNode } from "react"
+
+interface CardProps {
+  customStyle?: String,
+  children?: ReactNode,
+  number: number,
+  title: string,
+  content: string
+}
+
+const Card: React.FC<CardProps> = ({ customStyle = '', children, number, title, content }) => {
+  return (
+    <div className={`flex flex-col gap-7 w-[281px] items-center justify-center px-[26px] py-[56px]  
+      border-[1px] border-gradient`}>
+      <div className="flex items-center justify-center bg-[linear-gradient(97.58deg,#527FF7_-9.47%,#B251ED_24.58%,#F53E8E_93.49%,#FD6C5D_152.68%)] w-[69px] h-[39px] rounded-[44px]">
+        <p className="text-white text-[24px]">{number}</p>
+      </div>
+      <p className="text-white text-[20px]">{title}</p>
+      <p className="text-[14px] text-[#4A4465] leading-[16.5px] text-center">{content}</p>
+    </div>
+  );
+};
+
+export default Card;
